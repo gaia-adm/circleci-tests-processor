b7c609d93784d7ed94654b374c026e1ae7277c3b
Jaroslav Libak <jaroslav.libak@hp.com>
2015-08-18 11:12:51 +0200

    reformatted file

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -5,6 +5,7 @@ import unittest
 from tests.test_utils import execute_processor
 from testfixtures import compare
 
+
 def data_file_path(file_name):
     return os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', file_name))
 
@@ -12,7 +13,8 @@ def data_file_path(file_name):
 class TestProcessor(unittest.TestCase):
     def test_circleci_tests1(self):
         custom_metadata = {'SCM_BRANCH': 'master', 'SCM_REPO_NAME': 'jenkins-tests-processor',
-                           'SCM_URL': 'https://github.com/gaia-adm/jenkins-tests-processor', 'BUILD_URI': 'https://circleci.com/gh/gaia-adm/jenkins-tests-processor/12',
+                           'SCM_URL': 'https://github.com/gaia-adm/jenkins-tests-processor',
+                           'BUILD_URI': 'https://circleci.com/gh/gaia-adm/jenkins-tests-processor/12',
                            'BUILD_NUMBER': '12',
                            'BUILD_STATUS': 'success', 'BUILD_START_TIME': '2015-08-18T08:59:27.444Z'}
         output = execute_processor(data_file_path('circleci-tests1.json'), custom_metadata=custom_metadata)
